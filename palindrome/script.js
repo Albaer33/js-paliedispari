bc5f11bedc20486b5f86fd0ffef18544889f440c
Albaer33 <alisi3396@gmail.com>
2021-11-22 15:25:23 +0100

    palindroma finito

---
+++ palindrome/script.js
@@ -0,0 +1,23 @@
+// Chiedere all’utente di inserire una parola
+const userWord = prompt('inserisci una parola per verificare che sia palindroma:');
+let isPalindrome = false;
+isPalindrome = palindrome(userWord);
+if (isPalindrome) {
+    alert('Si, ' + userWord + ' è palindroma');
+}
+else {
+    alert('No, ' + userWord + ' non è palindroma');
+}
+// Creare una funzione per capire se la parola inserita è palindroma
+function palindrome(word) {
+    let reverseWord = [];
+    for (i = word.length-1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
+    if (word === reverseWord) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
